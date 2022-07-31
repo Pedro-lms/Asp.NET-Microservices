a4d758946ad305d2ac76bf79fc35c2e99dfb22d6
Pedro-lms <pedrodaluzmachado@gmail.com>
2022-07-30 23:04:25 -0300

    Controllers, Data and Repositories in Catalog MicroServices

---
+++ Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        IMongoCollection<Product> Products{ get; }
+
+        IMongoCollection<Product> ICatalogContext.Products => throw new NotImplementedException();
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeedBase.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeedBase
+    {
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+
+        }
+    }
+}
